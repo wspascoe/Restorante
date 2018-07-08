a88bf802578e7ef33213c6bc67107b8507f4fe6d
William Pascoe <wspascoe@gmail.com>
2018-07-08 19:27:08 -0400

    Menu Item Completed

--- Restorante/Controllers/MenuItemController.cs
+++ Restorante/Controllers/MenuItemController.cs
@@ -100,5 +100,145 @@ namespace Restorante.Controllers
 
             return Json(new SelectList(subCategoryList, "Id", "Name"));
         }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            MenuItemVM.MenuItem = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).SingleOrDefaultAsync(m=>m.Id == id);
+
+            MenuItemVM.SubCategory = _db.SubCategory.Where(s => s.CategoryId == MenuItemVM.MenuItem.CategoryId).ToList();
+
+            if(MenuItemVM.MenuItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(MenuItemVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id)
+        {
+            MenuItemVM.MenuItem.SubCategoryId = Convert.ToInt32(Request.Form["SubCategoryId"].ToString());
+
+            if (id != MenuItemVM.MenuItem.Id)
+            {
+                return NotFound();
+            }
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    string webRootPath = _hostingEnvironment.WebRootPath;
+                    var files = HttpContext.Request.Form.Files;
+                    var menuItemFromDb = _db.MenuItem.Where(m => m.Id == MenuItemVM.MenuItem.Id).FirstOrDefault();
+
+                    if(files[0].Length > 0 && files[0] != null)
+                    {
+                        var uploads = Path.Combine(webRootPath, "images");
+                        var extension_New = files[0].FileName.Substring(files[0].FileName.LastIndexOf("."), files[0].FileName.Length - files[0].FileName.LastIndexOf("."));
+
+                        var extension_Old = menuItemFromDb.Image.Substring(menuItemFromDb.Image.LastIndexOf("."), menuItemFromDb.Image.Length - menuItemFromDb.Image.LastIndexOf("."));
+
+                        if(System.IO.File.Exists(Path.Combine(uploads, MenuItemVM.MenuItem.Id + extension_Old)))
+                        {
+                            System.IO.File.Delete(Path.Combine(uploads, MenuItemVM.MenuItem.Id + extension_Old));
+                        }
+                        using (var filestream = new FileStream(Path.Combine(uploads, MenuItemVM.MenuItem.Id + extension_New), FileMode.Create))
+                        {
+                            files[0].CopyTo(filestream);
+                        }
+                        MenuItemVM.MenuItem.Image = @"\images\" + MenuItemVM.MenuItem.Id + extension_New;
+
+                    }
+
+                    if(MenuItemVM.MenuItem.Image != null)
+                    {
+                        menuItemFromDb.Image = MenuItemVM.MenuItem.Image;
+                    }
+                    menuItemFromDb.Name = MenuItemVM.MenuItem.Name;
+                    menuItemFromDb.Description = MenuItemVM.MenuItem.Description;
+                    menuItemFromDb.Price = MenuItemVM.MenuItem.Price;
+                    menuItemFromDb.Spicyness = MenuItemVM.MenuItem.Spicyness;
+                    menuItemFromDb.CategoryId = MenuItemVM.MenuItem.CategoryId;
+                    menuItemFromDb.SubCategoryId = MenuItemVM.MenuItem.SubCategoryId;
+
+                    await _db.SaveChangesAsync();
+
+                }
+                catch(Exception ex)
+                {
+
+                }
+                return RedirectToAction(nameof(Index));
+            }//is Valid
+            MenuItemVM.SubCategory = _db.SubCategory.Where(s=>s.CategoryId == MenuItemVM.MenuItem.CategoryId).ToList();
+
+            return View(MenuItemVM);
+        }//Edit
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            MenuItemVM.MenuItem = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).SingleOrDefaultAsync(m => m.Id == id);
+                       
+            if (MenuItemVM.MenuItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(MenuItemVM);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            MenuItemVM.MenuItem = await _db.MenuItem.Include(m => m.Category).Include(m => m.SubCategory).SingleOrDefaultAsync(m => m.Id == id);
+
+            if (MenuItemVM.MenuItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(MenuItemVM);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ConfirmDelete(int id)
+        {
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            MenuItem menuItem = await _db.MenuItem.FindAsync(id);
+
+            if(menuItem != null)
+            {
+                var uploads = Path.Combine(webRootPath, "images");
+                var extension = menuItem.Image.Substring(menuItem.Image.LastIndexOf("."), menuItem.Image.Length - menuItem.Image.LastIndexOf("."));
+
+                var imagePath = Path.Combine(uploads, menuItem.Id + extension);
+
+                if(System.IO.File.Exists(imagePath))
+                {
+                    System.IO.File.Delete(imagePath);
+                }
+                _db.MenuItem.Remove(menuItem);
+
+                await _db.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
